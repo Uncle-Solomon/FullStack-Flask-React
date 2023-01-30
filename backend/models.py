39924b7976f887be5665a7d8ddbe9b6112713161
Ameh Solomon Onyeke <amehsolomon46@gmail.com>
2023-01-30 16:07:45 +0100

    Initial Commit

---
+++ backend/models.py
@@ -0,0 +1,31 @@
+from exts import db
+
+class Recipe(db.Model):
+    id=db.Column(db.Integer(), primary_key=True)
+    title=db.Column(db.String(), nullable=False)
+    description=db.Column(db.Text(), nullable=False)
+
+    def __repr__(self):
+        return f"<Recipe {self.title} >"
+
+    
+    def save_recipe(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_recipe(self):
+        db.session.delete(self)
+        db.session.commit()
+    
+    def update_recipe(self, title, description):
+        self.title=title
+        self.description=description
+        db.session.commit()
+
+
+
+class User(db.Model):
+    id=db.Column(db.Integer(), primary_key=True)
+    username=db.Column(db.String(25), unique=True, nullable=False)
+    email=db.Column(db.String(85), unique=True, nullable=False)
+    password=db.Column(db.Text(255), nullable=False)
